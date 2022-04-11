466e48c9cdbadad0e322b6c9ba545bc183771ad5
shanto <santo15-10959@diu.edu.bd>
2022-04-12 02:42:56 +0600

    add displayname

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
+import useFirebase from '../../hooks/useFirebase';
 
 const Header = () => {
-
+    const {user, handleSignOut} = useFirebase();
     return (
         <div className='header'>
             <nav>
                 <Link to="/Home">Home</Link>
-                <Link to="/login">Login</Link>
+                <Link to="/products">Products</Link>
+                <Link to="/others">Others</Link>
+                <Link to="/register">Register</Link>
+                <span>{user?.displayName && user.displayName}</span>
+                {
+                    user?.uid
+                    ?
+                    <button onClick={handleSignOut}>Sign Out</button>
+                    :
+                    <Link to="/login">Login</Link>
+                }
             </nav>
         </div>
     );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 import './Login.css'
 
 const Login = () => {
+    const {signInWithGoogle} = useFirebase();
     return (
         <div>
-            <h3>please login</h3>
+            <h3>Please login</h3>
+            <div className='googleSungIn'>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
+            <form>
+                <input type="email" name=""  placeholder='Email'/>
+                <br />
+                <input type="password" name="" placeholder='Password'/>
+                <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
